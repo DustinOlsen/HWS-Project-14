a798473df8925b3cad30f1c7b49bd793da2dc186
DustinOlsen <69872283+DustinOlsen@users.noreply.github.com>
2021-04-08 09:49:56 -0500

    Initial Commit

---
+++ Shared/HWS_Project_14App.swift
@@ -0,0 +1,17 @@
+//
+//  HWS_Project_14App.swift
+//  Shared
+//
+//  Created by Dustin Olsen on 4/8/21.
+//
+
+import SwiftUI
+
+@main
+struct HWS_Project_14App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
