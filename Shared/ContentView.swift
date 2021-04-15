9fd23fdb545f33a0a22c99df0f23cf57c8b76b52
DustinOlsen <69872283+DustinOlsen@users.noreply.github.com>
2021-04-15 12:07:35 -0500

    HWS Project 14 - Day 70 - Part 1

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -6,63 +6,44 @@
 //
 
 import SwiftUI
-import LocalAuthentication
+import MapKit
 
 struct ContentView: View {
     
-    @State private var isUnlocked = false
-    
-    func authenticate() {
-        let context = LAContext()
-        var error: NSError?
-        
-        // check whether biometric auth is possible
-        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
-            // it's possible, so go ahead
-            let reason = "Unlock your data"
-            
-            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
-                // authentication complete
-                
-                DispatchQueue.main.async {
-                    if success {
-                        self.isUnlocked = true
-                    } else {
-                        // error
-                    }
-                }
-            }
-        } else {
-            // no biometrics
-        }
-        
-        
-    }
-//    func getDocumentsDirectory() -> URL {
-//        // find all possible documents directories for this user
-//        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-//
-//        // just send back the first one, which should be the only one
-//        return paths[0]
-//    }
-    
-    
+    @State private var centerCoordinate = CLLocationCoordinate2D()
+    @State private var locations = [MKPointAnnotation]()
     
     
     var body: some View {
         
-        VStack {
-            if self.isUnlocked {
-                Text("Unlocked")
-            } else {
-                Text("Locked")
+        ZStack {
+            MapView(centerCoordinate: $centerCoordinate, annotations: locations)
+                .edgesIgnoringSafeArea(.all)
+            Circle()
+                .fill(Color.blue)
+                .opacity(0.3)
+                .frame(width: 32, height: 32)
+            
+            VStack {
+                Spacer()
+                HStack {
+                    Spacer()
+                    Button(action: {
+                        let newLocation = MKPointAnnotation()
+                        newLocation.coordinate = self.centerCoordinate
+                        self.locations.append(newLocation)
+                    }) {
+                        Image(systemName: "plus")
+                    }
+                    .padding()
+                    .background(Color.black.opacity(0.75))
+                    .foregroundColor(.white)
+                    .font(.title)
+                    .clipShape(Circle())
+                    .padding(.trailing)
+                }
             }
         }
-        .onAppear(perform: authenticate)
-        
-//        MapView()
-//            .edgesIgnoringSafeArea(.all)
-            
     }
 }
 
--- Shared/MapView.swift
+++ Shared/MapView.swift
@@ -10,52 +10,54 @@ import MapKit
 
 struct MapView: UIViewRepresentable {
     
-    class Coordinator: NSObject, MKMapViewDelegate {
-        var parent: MapView
-        init(_ parent: MapView) {
-            self.parent = parent
-        }
-        func mapViewDidChangeVisibleRegion(_ mapView: MKMapView) {
-            print(mapView.centerCoordinate)
-        }
-        
-        func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-            let view = MKPinAnnotationView(annotation: annotation, reuseIdentifier: nil)
-            view.canShowCallout = true
-            return view
-        }
-            
+    
+    @Binding var centerCoordinate: CLLocationCoordinate2D
+    var annotations: [MKPointAnnotation]
+    
+    func makeUIView(context: Context) -> MKMapView {
+        let mapView = MKMapView()
+        mapView.delegate = context.coordinator
+        return mapView
+    }
+
+    func updateUIView(_ view: MKMapView, context: Context) {
+
+            if annotations.count != view.annotations.count {
+                view.removeAnnotations(view.annotations)
+                view.addAnnotations(annotations)
+            }
         
     }
-    
+
     func makeCoordinator() -> Coordinator {
         Coordinator(self)
     }
-    
-    func makeUIView(context: UIViewRepresentableContext<MapView>) -> MKMapView {
+
+    class Coordinator: NSObject, MKMapViewDelegate {
+        var parent: MapView
+
+        func mapViewDidChangeVisibleRegion(_ mapView: MKMapView) {
+            parent.centerCoordinate = mapView.centerCoordinate
+        }
         
-        let mapView = MKMapView()
-        mapView.delegate = context.coordinator
-        
+        init(_ parent: MapView) {
+            self.parent = parent
+        }
+    }
+}
+
+extension MKPointAnnotation {
+    static var example: MKPointAnnotation {
         let annotation = MKPointAnnotation()
         annotation.title = "London"
-        annotation.subtitle = "Capital of England"
-        annotation.coordinate = CLLocationCoordinate2D(latitude: 51.5, longitude: 0.13)
-        mapView.addAnnotation(annotation)
-        
-        return mapView
+        annotation.subtitle = "Home to the 2012 Summer Olympics."
+        annotation.coordinate = CLLocationCoordinate2D(latitude: 51.5, longitude: -0.13)
+        return annotation
     }
-    
-    func updateUIView(_ view: MKMapView, context: UIViewRepresentableContext<MapView>) {
-        
-    }
-    
-   
-    
 }
 
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
-        MapView()
+        MapView(centerCoordinate: .constant(MKPointAnnotation.example.coordinate), annotations: [MKPointAnnotation.example])
     }
 }
